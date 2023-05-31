91f344e5a233ce45bc342a47c69cc4b9405d569f
Geoffrey <126630931+Gstruss@users.noreply.github.com>
2023-05-30 23:16:55 -0500

    Create Buses.java

---
+++ Buses.java
@@ -0,0 +1,22 @@
+
+package taller.sipt.struss;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Geoffrey Struss
+ */
+public class Buses {
+
+    public static void main(String[] args) {
+        
+        System.out.println ("Bienvenido a SIPT SAS"); 
+      
+        Menu menu = new Menu();
+  
+         menu.menu();
+        
+    }
+    
+}
